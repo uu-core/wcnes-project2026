125099f9f07d9af28af8b19e2703aa2450bd2533
Albin Kjellson <albkj407@student.liu.se>
2026-05-04 13:27:36 +0200

    Added led indicator when sending packets from tag, changed header length for shorter syncword experiment

--- carrier-receiver-baseband/flash.sh
+++ carrier-receiver-baseband/flash.sh
@@ -3,5 +3,5 @@ sleep 5;
 picotool load build/carrier_receiver_baseband.elf; # Load the elf file
 picotool reboot;
 sleep 2;
-picocom -b 115200 /insert/your/path; # Connect to see the output CTRL+A CTRL+X to exit picocom
+picocom -b 115200 /dev/ttyACM0; # Connect to see the output CTRL+A CTRL+X to exit picocom
 
--- carrier-receiver-baseband/main.c
+++ carrier-receiver-baseband/main.c
@@ -34,7 +34,7 @@
 #define RADIO_SCK               18
 
 #define TX_DURATION            250 // send a packet every 250ms (when changing baud-rate, ensure that the TX delay is larger than the transmission time)
-#define RECEIVER              2500 // define the receiver board either 2500 or 1352
+#define RECEIVER              1352 // define the receiver board either 2500 or 1352
 #define PIN_TX1                  6
 #define PIN_TX2                 27
 #define CLOCK_DIV0              20 // larger
@@ -101,6 +101,12 @@ int main() {
     set_filter_bandwidth_rx(backscatter_conf.minRxBw);
     sleep_ms(1);
     RX_start_listen();
+
+    // LED indication that listening has started
+    gpio_init(25);
+    gpio_set_dir(25, GPIO_OUT);
+    gpio_put(25, 1);
+
     printf("started listening\n");
     bool rx_ready = true;
 
